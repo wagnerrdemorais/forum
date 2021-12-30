191b90d948829da285bed1c6f3b86da028394237
Wagner Rodakevicz de Morais <wagnerrdemorais@gmail.com>
2021-12-30 16:14:15 -0300

    adicionando dockerfilee

--- src/main/java/com/wagnerrdemorais/forum/ForumApplication.java
+++ src/main/java/com/wagnerrdemorais/forum/ForumApplication.java
@@ -2,7 +2,6 @@ package com.wagnerrdemorais.forum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
@@ -14,15 +13,10 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 
 public class ForumApplication
-        extends SpringBootServletInitializer
-{
+        extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(ForumApplication.class, args);
     }
 
-//    @Override
-//    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-//        return builder.sources(ForumApplication.class);
-//    }
 }
