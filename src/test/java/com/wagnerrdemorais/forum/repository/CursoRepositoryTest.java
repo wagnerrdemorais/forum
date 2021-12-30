a2d9c4d2a6de21fa1769ac5de36034385de4615d
Wagner Rodakevicz de Morais <wagnerrdemorais@gmail.com>
2021-12-30 15:07:37 -0300

    adicionando outra abordagem para realizar testes de repositório

--- src/test/java/com/wagnerrdemorais/forum/repository/CursoRepositoryTest.java
+++ src/test/java/com/wagnerrdemorais/forum/repository/CursoRepositoryTest.java
@@ -5,17 +5,32 @@ import io.jsonwebtoken.lang.Assert;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
 
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE) // nao substituir o banco pelo h2
+@ActiveProfiles("test")
 public class CursoRepositoryTest {
 
     @Autowired
     private CursoRepository repository;
 
+    @Autowired
+    private TestEntityManager testEntityManager;
+
     @Test
     public void deveriaCarregarCursoAoBuscarPeloNome() {
         String nomeCurso = "HTML 5";
+
+        Curso curso = new Curso();
+        curso.setNome(nomeCurso);
+        curso.setCategoria("Programacao");
+
+        testEntityManager.persist(curso);
+
         Curso byNome = repository.findByNome(nomeCurso);
 
         Assert.notNull(byNome);
